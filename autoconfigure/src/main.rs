acd621469821bf10ba6a8dab21e6ce2929b346e5
aorps <aorbeandrews@gmail.com>
2024-01-04 14:05:07 -0500

    trial of clap cli lib
    Signed-off-by: aorps <aorbeandrews@gmail.com>

--- autoconfigure/src/main.rs
+++ autoconfigure/src/main.rs
@@ -1,10 +1,27 @@
+use clap::Parser;
 use gtmpl;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    // Name of the person to greet
+    #[arg(short,long)]
+    name: String,
 
+    // Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
 
 fn main() {
-    let output = gtmpl::template("Finally, some template {{ . }} in rust", "nuts");
-    assert_eq!(&output.unwrap(), "Finally, some template nuts in rust");
-    println!("Hello, world!");
+    let args = Args::parse();
+    for _ in 0..args.count {
+	println!("Hello {}!", args.name);
+    }
+    
 
+    let output = gtmpl::template("Finally, some template {{ . }} in rust", "nuts");
+    // assert_eq!(&output.unwrap(), "Finally, some template nuts in rust");
+    println!("Hello, world!");
+    println!("{:?}", &output.unwrap());
 }
