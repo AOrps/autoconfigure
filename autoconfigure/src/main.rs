1860130bd0bcf672ae2fee54f6bf70dce478830c
aorps <aorbeandrews@gmail.com>
2024-01-04 01:57:38 -0500

    rust formatting
    Signed-off-by: aorps <aorbeandrews@gmail.com>

--- autoconfigure/src/main.rs
+++ autoconfigure/src/main.rs
@@ -1,8 +1,10 @@
 use gtmpl;
 
+
+
 fn main() {
-   let output = gtmpl::template("Finally, some template {{ . }} in rust", "nuts");
-   assert_eq!(&output.unwrap(), "Finally, some template nuts in rust");
-   
-   println!("Hello, world!");
+    let output = gtmpl::template("Finally, some template {{ . }} in rust", "nuts");
+    assert_eq!(&output.unwrap(), "Finally, some template nuts in rust");
+    println!("Hello, world!");
+
 }
